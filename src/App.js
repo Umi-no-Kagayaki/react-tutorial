b11fb42675ce52fd455dbb4d59bb030cc8cffb86
平田海輝 <seaglory2004@gmail.com>
2021-01-16 15:48:04 +0900

    add new chapter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
+import {useState} from 'react';
+import {List} from './List';
+import {Form} from './Form'
+
 function App() {
+  const [tab, setTab] = useState('list');
+
   return (
-    <div>ゼロから始めるReact入門</div>
+    <div>
+      <header>
+        <ul>
+          <li onClick = {() => setTab('list')}>リスト</li>
+
+          <li onClick = {() => setTab('form')}>フォーム</li>
+        </ul>
+      </header>
+
+      <hr/>
+
+      {
+        tab === 'list' ? <List /> : <Form />
+      }
+    </div>
   );
 }
 
